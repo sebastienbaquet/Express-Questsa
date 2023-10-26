0547da5901d64d92db9289cc7a6c31f3dfacbee6
sebastien b <bas.envoi2604@gmail.com>
2023-10-26 20:08:29 +0200

    users_ok

--- src/app.js
+++ src/app.js
@@ -1,10 +1,18 @@
+require("dotenv").config();
 const express = require("express");
+
 
 const app = express();
 
-const movieControllers = require("./controllers/movieControllers");
+const movieControllers = require("../movieControllers");
 
 app.get("/api/movies", movieControllers.getMovies);
 app.get("/api/movies/:id", movieControllers.getMovieById);
+
+app.get("/api/users", movieControllers.getUsers);
+app.get("/api/users/:id", movieControllers.getUsersById);
+
+
+
 
 module.exports = app;
