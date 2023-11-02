f7a9c7d7b1abd20f9d1f0eb341431a3217e57168
sebastien b <bas.envoi2604@gmail.com>
2023-11-02 17:08:00 +0100

    express3

--- movieControllers.js
+++ movieControllers.js
@@ -1,6 +1,5 @@
 require("dotenv").config();
 const database = require("./database");
-
 const movies = [
   {
     id: 1,
@@ -30,68 +29,101 @@ const movies = [
 
 const getMovies = (req, res) => {
   database
-  .query("select * from movies")
-  .then(([movies]) => {
-    res.json(movies);
-  })
-  .catch((err) => {
-    console.error(err);
-    res.sendstatus(500);
-  });
-  
+    .query("select * from movies")
+    .then(([movies]) => {
+      res.json(movies);
+    })
+    .catch((err) => {
+      console.error(err);
+      res.sendStatus(500);
+    });
 };
-
 const getMovieById = (req, res) => {
   const id = parseInt(req.params.id);
 
   database
-  .query("select * from movies where id = ?", [id])
-  .then(([movies]) => {
-    if (movies[0] != null) {
-      res.json(movies[0]);
-    } else {
-      res.sendStatus(404);
-    }
-  })
-  .catch((err) => {
-    console.error(err);
-    res.sendStatus(500);
-  });
+    .query("select * from movies where id = ?", [id])
+    .then(([movies]) => {
+      if (movies[0] != null) {
+        res.json(movies[0]);
+      } else {
+        res.sendStatus(404);
+      }
+    })
+    .catch((err) => {
+      console.error(err);
+      res.sendStatus(500);
+    });
 };
 
 const getUsers = (req, res) => {
   database
-  .query("select * from users")
-  .then(([users]) => {
-    res.status(200).json(users);
-  })
-  .catch((err) => {
-    console.error(err);
-    res.sendstatus(500);
-  });
-}
+    .query("select * from users")
+    .then(([users]) => {
+      res.status(200).json(users);
+    })
+    .catch((err) => {
+      console.error(err);
+      res.sendStatus(500);
+    });
+};
 
 const getUsersById = (req, res) => {
   const id = parseInt(req.params.id);
 
   database
-  .query("select * from users where id = ?", [id])
-  .then(([users]) => {
-    if (users[0] != null) {
-      res.json(users[0]);
-    } else {
-      res.sendStatus(404);
-    }
-  })
-  .catch((err) => {
-    console.error(err);
-    res.sendStatus(500);
-  });
+    .query("select * from users where id = ?", [id])
+    .then(([users]) => {
+      if (users[0] != null) {
+        res.json(users[0]);
+      } else {
+        res.sendStatus(404);
+      }
+    })
+    .catch((err) => {
+      console.error(err);
+      res.sendStatus(500);
+    });
 };
+
+const postMovie = (req, res) => {
+  const { title, director, year, color, duration } = req.body;
+
+  database
+    .query(
+      "INSERT INTO movies(title, director, year, color, duration) VALUES (?, ?, ?, ?, ?)",
+      [title, director, year, color, duration]
+    )
+    .then(([result]) => {
+      res.status(201).send({ id: result.insertId });
+    })
+    .catch((err) => {
+      console.error(err);
+      res.sendStatus(500);
+    });
+};
+const postUser = (req, res) => {
+  const { firstname, lastname, email, city, language } = req.body;
+
+  database
+    .query(
+      "INSERT INTO users(firstname, lastname, email, city, language) VALUES (?, ?, ?, ?, ?)",
+      [firstname, lastname, email, city, language]
+    )
+    .then(([result]) => {
+      res.status(201).send({ id: result.insertId });
+    })
+    .catch((err) => {
+      console.error(err);
+      res.sendStatus(500);
+    });
+};
+
 module.exports = {
   getMovies,
   getMovieById,
-getUsers,
-getUsersById,
-
+  getUsers,
+  getUsersById,
+  postMovie,
+  postUser,
 };
--- src/app.js
+++ src/app.js
@@ -3,7 +3,7 @@ const express = require("express");
 
 
 const app = express();
-
+app.use(express.json());
 const movieControllers = require("../movieControllers");
 
 app.get("/api/movies", movieControllers.getMovies);
@@ -12,6 +12,8 @@ app.get("/api/movies/:id", movieControllers.getMovieById);
 app.get("/api/users", movieControllers.getUsers);
 app.get("/api/users/:id", movieControllers.getUsersById);
 
+app.post("/api/movies", movieControllers.postMovie);
+app.post("/api/users", movieControllers.postUser);
 
 
 
